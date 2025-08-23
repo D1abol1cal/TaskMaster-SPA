e9966258e4ba33f854eaa105a808a9c44bbadb0a
Nofel <syednofeltalha2@gmail.com>
2025-08-23 21:19:52 +0500

    Initial commit

---
+++ taskmasterbackend/src/main/java/com/taskmaster/taskmasterbackend/TaskmasterbackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskmaster.taskmasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmasterbackendApplication.class, args);
+    }
+
+}
