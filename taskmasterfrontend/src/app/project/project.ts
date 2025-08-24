a99353af2e766e6c403b6541386a98b9bfb44217
Nofel <syednofeltalha2@gmail.com>
2025-08-24 05:05:58 +0500

    static UI done

--- taskmasterfrontend/src/app/project/project.ts
+++ taskmasterfrontend/src/app/project/project.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {ProjectTitle} from './project-title/project-title';
+import {TaskList} from './task-list/task-list';
+import {ProgressBar} from './progress-bar/progress-bar';
 
 @Component({
   selector: 'app-project',
-  imports: [],
+  imports: [
+    ProjectTitle,
+    TaskList,
+    ProgressBar
+  ],
   templateUrl: './project.html',
   styleUrl: './project.css'
 })
